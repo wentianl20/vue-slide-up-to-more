7b95971b686760209cb68fdb417d4dde64824a5c
Pirlo Liu <wentianliu@aliyun.com>
2022-10-25 16:19:31 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { Cell, CellGroup } from 'vant';
 import 'vant/lib/index.css';
-import vueSlideUpToMore from './components/index'
+// import vueSlideUpToMore from './components/index'
+import PageList from 'vue-slide-up-to-more'
 
 const app = createApp(App)
 app.use(Cell).use(CellGroup).use(vueSlideUpToMore).mount('#app')
